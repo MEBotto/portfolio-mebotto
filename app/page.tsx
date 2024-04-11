16360e3f3afb18443d239b8bbb54ff98e0054045
Mariano Botto <marianobotto@gmail.com>
2024-04-11 07:26:49 -0300

    style: change home image

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-lg">
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
         <Image
-          src={"/hero.png"}
+          src={"/profile.png"}
           alt="Profile image"
           fill
           className="object-contain"
