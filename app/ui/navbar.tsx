fcddf2d927f5507aadaece8eefe0e0bf6f067bb4
Mariano Botto <marianobotto@gmail.com>
2024-04-10 21:00:39 -0300

    fix: fix menu button when open the menu

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import NavLink from "./navlink";
+import { motion } from "framer-motion";
 
 const links = [
   { url: "/", title: "Home" },
@@ -15,14 +16,42 @@ const links = [
 export default function Navbar() {
   const [open, setOpen] = useState(false);
 
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+
+  const centerVariants = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  };
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-lg">
       <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
-          <NavLink key={link.title} link={link}/>
+          <NavLink key={link.title} link={link} />
         ))}
       </div>
-      {/* LOGO */}
       <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href={"/"}
@@ -34,30 +63,48 @@ export default function Navbar() {
           </span>
         </Link>
       </div>
-      {/* SOCIAL */}
       <div className="hidden md:flex gap-4 w-1/3 justify-end">
         <Link href={"https://github.com/MEBotto"}>
-          <Image src={"/github.png"} alt="Github Logo" width={24} height={24}/>
+          <Image src={"/github.png"} alt="Github Logo" width={24} height={24} />
         </Link>
         <Link href={"https://instagram.com/mariano_botto03/"}>
-          <Image src={"/instagram.png"} alt="Instagram Logo" width={24} height={24}/>
+          <Image
+            src={"/instagram.png"}
+            alt="Instagram Logo"
+            width={24}
+            height={24}
+          />
         </Link>
         <Link href={"https://linkedin.com/in/marianobotto/"}>
-          <Image src={"/linkedin.png"} alt="Linkedin Logo" width={24} height={24}/>
+          <Image
+            src={"/linkedin.png"}
+            alt="Linkedin Logo"
+            width={24}
+            height={24}
+          />
         </Link>
       </div>
-      {/* RESPONSIVE MENU */}
       <div className="md:hidden">
-        {/* MENU BUTTON */}
         <button
           className="w-10 h-8 flex flex-col justify-between z-50 relative"
           onClick={() => setOpen(!open)}
         >
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
+          <motion.div
+            variants={topVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
+          <motion.div
+            variants={centerVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded"
+          ></motion.div>
+          <motion.div
+            variants={bottomVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
         </button>
-        {/* MENU LIST */}
         {open && (
           <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
             {links.map((link) => (
