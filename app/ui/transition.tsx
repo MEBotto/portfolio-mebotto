1899d588ce35cd4133249702872923308639ba58
Mariano Botto <marianobotto@gmail.com>
2024-04-11 13:18:13 -0300

    fix: fix transition text

--- app/ui/transition.tsx
+++ app/ui/transition.tsx
@@ -26,6 +26,7 @@ export default function Transition({
         <motion.div
           className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit"
           initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
         >
