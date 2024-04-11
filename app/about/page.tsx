45e727e2b4d3f169269d6e3da4ff5f7085803465
Mariano Botto <marianobotto@gmail.com>
2024-04-11 16:02:25 -0300

    feat: add sections to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,14 @@ export default function About() {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <p>About Page</p>
+      <div className="">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24">
+          <div className="">BIOGRAPHY</div>
+          <div className="">SKILLS</div>
+          <div className="">EXPERIENCE</div>
+        </div>
+        <div className="hidden"></div>
+      </div>
     </motion.div>
   );
 }
