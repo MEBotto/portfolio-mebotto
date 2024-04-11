0a4e1565088b7e40bff16137b4f65889eb7d4f7b
Mariano Botto <marianobotto@gmail.com>
2024-04-11 12:26:45 -0300

    feat: Add transition when change pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./ui/navbar";
+import Transition from "./ui/transition";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
-          <div className="h-24">
-            <Navbar />
-          </div>
-          <div className="h-[calc(100vh-6rem)]">{children}</div>
-        </div>
+        <Transition>{children}</Transition>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,43 @@
+"use client";
+
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-lg">
-      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-        <Image
-          src={"/profile.png"}
-          alt="Profile image"
-          fill
-          className="object-contain"
-        />
-      </div>
-      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center lg:items-start">
-        <h1 className="text-4xl md:text-6xl font-bold">Title</h1>
-        <p className="md:text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-          suscipit recusandae nulla doloremque incidunt, fugiat veniam nihil a
-          velit debitis cupiditate nemo fugit at quod repellendus vel tempora,
-          dolores consequatur?
-        </p>
-        <div className="flex gap-4">
-          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
-          <button className="p-4 rounded-lg ring-1 ring-black">Contact Me</button>
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-lg">
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image
+            src={"/profile.png"}
+            alt="Profile image"
+            fill
+            className="object-contain"
+          />
+        </div>
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center lg:items-start">
+          <h1 className="text-4xl md:text-6xl font-bold">Title</h1>
+          <p className="md:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            suscipit recusandae nulla doloremque incidunt, fugiat veniam nihil a
+            velit debitis cupiditate nemo fugit at quod repellendus vel tempora,
+            dolores consequatur?
+          </p>
+          <div className="flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Work
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
